33760ed919664df6abd8f9a343cc5883ff149ed7
maily-vannobel <maily.vannobel@laplateforme.io>
2024-10-23 10:44:14 +0200

    feat: add routes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import recipeRoutes from './routes/recipeRoutes.js';
+import ingredientRoutes from './routes/ingredientRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,16 +11,27 @@ app.use(helmet());
 app.set('view engine', 'ejs');
 app.use(express.static('assets'));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Page d'accueil
 app.get('/', (req, res) => {
     res.render('home'); 
 });
 
-// routes des recettes
-import recipeRoutes from './routes/recipeRoutes.js';
+// Routes des recettes
 app.use('/recipes', recipeRoutes);
 
-// Lancer le serveur
+// Routes des ingrédients
+app.use('/ingredients', ingredientRoutes);
+
+// Gestion des erreurs 404
+app.use((req, res) => {
+    res.status(404).send("Page non trouvée");
+});
+
 app.listen(PORT, () => {
     console.log(`CA TOURNE SUR ${PORT}`);
 });
+
+export default app;
