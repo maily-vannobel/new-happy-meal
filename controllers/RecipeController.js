1601758d99a5250d47512dcdb191cee492ea4cf0
maily-vannobel <maily.vannobel@laplateforme.io>
2024-10-24 21:35:37 +0200

    refactor: use EJS templates

--- controllers/RecipeController.js
+++ controllers/RecipeController.js
@@ -1,38 +1,46 @@
 import RecipeModel from '../models/RecipeModel.js';
-import IngredientModel from '../models/IngredientModel.js'; // Importation du modèle des ingrédients
+import IngredientModel from '../models/IngredientModel.js'; 
 
 class RecipeController {
-    
+
     // 1: Afficher toutes les recettes
     static async index(req, res) {
         try {
             const recipes = await RecipeModel.getAllRecipes();
-            res.render('recipe', { recipes });
+            res.render('layouts/base', {
+                title: 'Recettes',
+                body: 'recipes',
+                cssFile: 'recipes',
+                recipes: recipes
+            });
         } catch (error) {
             console.error('Erreur lors de la récupération des recettes :', error);
             res.status(500).send('Erreur lors de la récupération des recettes');
         }
     }
 
-    // 2: Afficher détails d'une recette par ID
-    static async show(req, res) {
-        try {
-            const recipe = await RecipeModel.getRecipeById(req.params.id);
-            if (!recipe) {
-                return res.status(404).send('Recette non trouvée');
-            }
-
-            // Récupérer les ingrédients associés à la recette
-            const ingredients = await IngredientModel.getIngredientsByRecipe(req.params.id);
-
-            // Passer la recette et les ingrédients à la vue
-            res.render('recipeDetails', { recipe, ingredients });
-        } catch (error) {
-            console.error('Erreur lors de la récupération de la recette :', error);
-            res.status(500).send('Erreur lors de la récupération de la recette');
+// 2: Afficher détails d'une recette par ID
+static async show(req, res) {
+    try {
+        const recipe = await RecipeModel.getRecipeById(req.params.id);
+        if (!recipe) {
+            return res.status(404).send('Recette non trouvée');
         }
-    }
 
+        const ingredients = await IngredientModel.getIngredientsByRecipe(req.params.id);
+
+        res.render('layouts/base', {
+            title: recipe.name,
+            body: 'recipeDetails', 
+            recipe: recipe, 
+            ingredients: ingredients, 
+            cssFile: 'recipeDetails'
+        });
+    } catch (error) {
+        console.error('Erreur lors de la récupération de la recette :', error);
+        res.status(500).send('Erreur lors de la récupération de la recette');
+    }
+}
     // 3: Recherche des recettes pour l'autocomplétion
     static async search(req, res) {
         try {
